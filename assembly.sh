4c908ced6755ce4cdd0e2c772ac9e3a6bc3e08e2
kohsuke <null>
2006-09-22 18:17:54 +0000

    renamed to the standard command name.

--- assembly.sh
+++ assembly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 #
 # create a distribution package
-maven2 -o clean package
+mvn -o clean package
 pushd cli
-maven2 -o assembly:assembly
+mvn -o assembly:assembly
 popd
-maven2 -o assembly:assembly
+mvn -o assembly:assembly
