10fc5c773911cfaee63bc351da1781d5bb3e0872
kohsuke <null>
2006-09-24 01:09:26 +0000

    fixed a bug where a token with numbers in it fails to produce a valid link.

---
+++ core/src/test/java/T0k4n.java
@@ -0,0 +1,68 @@
+/*
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+long head
+*/
+
+/**
+ * token with numbers in it.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class T0k4n {
+    //byte[] foo(byte[] x) {
+    //    return x;
+    //}
+    //
+    public String a12_$_aaa(byte[] val) {
+        return new String(val);
+    }
+
+}
+
+/*
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+long tail
+*/
