0091d6a2c889669e233ec6531b5d937c2235b711
kohsuke <null>
2007-07-13 12:25:52 +0000

    working around SUREFIRE-300.

---
+++ cli/src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+/**
+ * Work around SUREFIRE-300.
+ * 
+ * @author Kohsuke Kawaguchi
+ */
+public class DummyTest extends TestCase {
+    public void test1() {}
+}
